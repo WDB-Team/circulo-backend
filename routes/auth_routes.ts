6f01e4af07c2763c66d83739158bb4e3261c08e2
Luis Felipe <luisfelipemillanprado@gmail.com>
2022-06-22 00:28:40 -0400

    eliminando los campos little description y residencia3

--- routes/auth_routes.ts
+++ routes/auth_routes.ts
@@ -12,98 +12,74 @@ import { Types } from "mongoose";
 require("../security/basic.js");
 const usersystem_controller: UserSystemController = new UserSystemController();
 const router: Router = express.Router();
-router.get(
-	"/inicio",
-	async function (request: Request, response: Response, next: NextFunction) {
-		response.send("hola llego...!");
-	},
-);
+router.get("/inicio", async function (request: Request, response: Response, next: NextFunction) {
+	response.send("hola llego...!");
+});
 /**
  * @description:
  */
-router.get(
-	"/sign-in",
-	async function (request: Request, response: Response, next: NextFunction) {
-		passport.authenticate(
-			"basic",
-			{ session: false },
-			function (
-				error: any,
-				user: {
-					_id: Types.ObjectId;
-					full_name: string;
-					email: string;
-					password: string | any;
-					little_description: string;
-				},
-			) {
-				try {
-					if (error || !user) {
-						responses.Errors(
-							request,
-							response,
-							boom.unauthorized().message,
-							400,
-						);
-					}
-					const token: string = jwt.sign(
-						{
-							user_id: user._id,
-							email: user.email,
-							full_name: user.full_name,
-						},
-						config.JWT_SECRET.authJwtSecret,
-						{ expiresIn: "90d" },
-					);
-					responses.Success(request, response, [user._id, token], 200);
-				} catch (error: any) {
-					next(error);
-				}
+router.get("/sign-in", async function (request: Request, response: Response, next: NextFunction) {
+	passport.authenticate(
+		"basic",
+		{ session: false },
+		function (
+			error: any,
+			user: {
+				_id: Types.ObjectId;
+				full_name: string;
+				email: string;
+				password: string | any;
+				little_description: string;
 			},
-		)(request, response, next);
-	},
-);
-/**
- * @description:
- */
-router.post(
-	"/sign-up",
-	async function (request: Request, response: Response, next: NextFunction) {
-		if (
-			!request.body.full_name ||
-			!request.body.email ||
-			!request.body.password ||
-			!request.body.little_description
 		) {
-			responses.Errors(
-				request,
-				response,
-				"Envio de informacion invalida...!",
-				400,
-			);
-		} else {
-			const transaction: {
-				successfully_transaction?: boolean;
-				user_id?: any;
-				error?: any;
-			} = await usersystem_controller.addUserSystem(request.body);
-			if (transaction.user_id) {
+			try {
+				if (error || !user) {
+					responses.Errors(request, response, boom.unauthorized().message, 400);
+				}
 				const token: string = jwt.sign(
 					{
-						user_id: transaction.user_id,
-						email: request.body.email,
-						full_name: request.body.full_name,
+						user_id: user._id,
+						email: user.email,
+						full_name: user.full_name,
 					},
 					config.JWT_SECRET.authJwtSecret,
 					{ expiresIn: "90d" },
 				);
-				responses.Success(request, response, [transaction.user_id, token], 201);
+				responses.Success(request, response, [user._id, token], 200);
+			} catch (error: any) {
+				next(error);
 			}
-			if (transaction.error) {
-				next(transaction.error);
-			}
+		},
+	)(request, response, next);
+});
+/**
+ * @description:
+ */
+router.post("/sign-up", async function (request: Request, response: Response, next: NextFunction) {
+	if (!request.body.full_name || !request.body.email || !request.body.password) {
+		responses.Errors(request, response, "Envio de informacion invalida...!", 400);
+	} else {
+		const transaction: {
+			successfully_transaction?: boolean;
+			user_id?: any;
+			error?: any;
+		} = await usersystem_controller.addUserSystem(request.body);
+		if (transaction.user_id) {
+			const token: string = jwt.sign(
+				{
+					user_id: transaction.user_id,
+					email: request.body.email,
+					full_name: request.body.full_name,
+				},
+				config.JWT_SECRET.authJwtSecret,
+				{ expiresIn: "90d" },
+			);
+			responses.Success(request, response, [transaction.user_id, token], 201);
 		}
-	},
-);
+		if (transaction.error) {
+			next(transaction.error);
+		}
+	}
+});
 //--
 export default router;
